2d66621fe8b7cfb53fbae51cf644ffc2ce54d8da
Sergiy <sergey.morenets@gmail.com>
2022-08-13 11:07:31 +0300

    Task 1

--- persistence-task1/src/main/java/it/discovery/persistence/model/Publisher.java
+++ persistence-task1/src/main/java/it/discovery/persistence/model/Publisher.java
@@ -1,10 +1,14 @@
 package it.discovery.persistence.model;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * Book publisher
@@ -13,11 +17,15 @@ import lombok.Setter;
  */
 @Getter
 @Setter
+@Entity
+@Table
 public class Publisher {
+    @Id
     private int id;
 
     private String name;
 
+    @Transient
     private List<Book> books;
 
     private LocalDateTime created;
