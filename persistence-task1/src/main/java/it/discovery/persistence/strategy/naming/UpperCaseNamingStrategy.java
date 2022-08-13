e9093e4a9753b305b3edaf6d2f1d1944bb8b355c
Sergiy <sergey.morenets@gmail.com>
2022-08-13 13:24:23 +0300

    Task 3.1. Naming strategy

---
+++ persistence-task1/src/main/java/it/discovery/persistence/strategy/naming/UpperCaseNamingStrategy.java
@@ -0,0 +1,31 @@
+package it.discovery.persistence.strategy.naming;
+
+import org.hibernate.boot.model.naming.Identifier;
+import org.hibernate.boot.model.naming.PhysicalNamingStrategyStandardImpl;
+import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
+
+public class UpperCaseNamingStrategy extends PhysicalNamingStrategyStandardImpl {
+
+    @Override
+    public Identifier toPhysicalTableName(Identifier logicalName, JdbcEnvironment context) {
+        return Identifier.toIdentifier(convertToUpperCase(logicalName.getText()), logicalName.isQuoted());
+    }
+
+    @Override
+    public Identifier toPhysicalColumnName(Identifier logicalName, JdbcEnvironment context) {
+        return Identifier.toIdentifier(convertToUpperCase(logicalName.getText()), logicalName.isQuoted());
+    }
+
+    private String convertToUpperCase(String name) {
+        StringBuilder builder = new StringBuilder(name.length());
+        for (int i = 0; i < name.length(); i++) {
+            char ch = name.charAt(i);
+            if (i != 0 && Character.isUpperCase(ch)) {
+                builder.append('_');
+            }
+            builder.append(Character.toUpperCase(ch));
+        }
+
+        return builder.toString();
+    }
+}
